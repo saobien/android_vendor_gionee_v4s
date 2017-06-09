7a234c3f66523008a49927a257137e5759719f09
MinhQuoc <saobien.me@gmail.com>
2017-06-09 22:31:33 +0700

    Add files via upload

---
+++ v4s-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/v4s/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/v4s/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/v4s/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/v4s/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/v4s/proprietary/vendor,system/vendor)
